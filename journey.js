5450616021267d0c02ea05aaec8b77c5a1fc8abc
Kartikey Kanaujiya <Kartikey7905@gmail.com>
2026-06-13 14:31:01 +0530

    Added professional domains and events pages with styling

---
+++ journey.js
@@ -0,0 +1,40 @@
+const slider = document.querySelector(".slider");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+if (slider && nextBtn && prevBtn) {
+
+    const scrollAmount = 525;
+
+    nextBtn.addEventListener("click", () => {
+        slider.scrollBy({
+            left: scrollAmount,
+            behavior: "smooth"
+        });
+    });
+
+    prevBtn.addEventListener("click", () => {
+        slider.scrollBy({
+            left: -scrollAmount,
+            behavior: "smooth"
+        });
+    });
+
+    setInterval(() => {
+        if (
+            slider.scrollLeft + slider.clientWidth >=
+            slider.scrollWidth - 10
+        ) {
+            slider.scrollTo({
+                left: 0,
+                behavior: "smooth"
+            });
+        } else {
+            slider.scrollBy({
+                left: scrollAmount,
+                behavior: "smooth"
+            });
+        }
+    }, 3000);
+
+}
